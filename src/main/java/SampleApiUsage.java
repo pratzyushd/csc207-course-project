614e7d724d33cc462a7b3bdf6968452a3acd6444
Pratyush Dwivedi <7183103-pratzyushd@users.noreply.gitlab.com>
2023-09-30 03:43:08 -0400

    Update gitignore file

---
+++ src/main/java/SampleApiUsage.java
@@ -0,0 +1,10 @@
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SampleApiUsage {
+    public static void main(String[]args){
+            System.out.println("Hello World");
+    }
+}
