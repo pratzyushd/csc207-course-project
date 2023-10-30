4b30d226460cf6fe87e8c8a67ea067f43cce31b1
Jaeyong Lee <lee.j1533@gmail.com>
2023-10-30 14:41:35 -0400

    add recipe entity and recipe entity factory

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -1,4 +1,13 @@
 package entity;
 
 public interface Recipe {
+    int getMealId();
+
+    String getName();
+
+    String getCategory();
+
+    String getInstructions();
+
+    String getAreaOfOrigin();
 }
