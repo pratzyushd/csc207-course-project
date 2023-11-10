7eafeddae5268486c4f798046d224c34fd076eed
Pratyush Dwivedi <7183103-pratzyushd@users.noreply.gitlab.com>
2023-11-10 17:02:37 -0500

    Complete searchRecipeByCuisine method; some additional refactoring of common functionality

--- src/data_access/TheMealDB.java
+++ src/data_access/TheMealDB.java
@@ -10,6 +10,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TheMealDB implements RecipeAPI {
 
@@ -29,30 +32,21 @@ public class TheMealDB implements RecipeAPI {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            if (response.body() == null) {
-                return null;
-            }
-            if (response.code() != 200) {
+            if (!responseIsValid(response)) {
                 return null;
             }
 
             JSONObject responseBody = new JSONObject(response.body().string());
 
             /* We get back an empty list i.e. the ID doesn't exist in TheMealDB. */
-            if (responseBody.isNull("meals")) {
+            if  (!dataInAPIResponse(responseBody)) {
                 return null;
             }
 
-            System.out.println(responseBody);
             /* The API returns an array with only one item, so we know that the first item is the one we want. */
             JSONArray mealArray = responseBody.getJSONArray("meals");
             JSONObject meal = mealArray.getJSONObject(0);
-            return this.recipeFactory.create(
-                    meal.getInt("idMeal"),
-                    meal.getString("strMeal"),
-                    meal.getString("strCategory"),
-                    meal.getString("strInstructions"),
-                    meal.getString("strArea"));
+            return constructRecipeFromMealJSON(meal);
 
         } catch (IOException | JSONException e) {
             return null;
@@ -61,11 +55,96 @@ public class TheMealDB implements RecipeAPI {
 
     @Override
     public Recipe[] searchRecipesByCuisine(String cuisine) {
-        return new Recipe[0];
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url(String.format(API_URL + "filter.php?a=" + cuisine))
+                .addHeader("Content-type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!responseIsValid(response)) {
+                return null;
+            }
+
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            /* We get back an empty list i.e. the ID doesn't exist in TheMealDB. */
+            if (!dataInAPIResponse(responseBody)) {
+                return null;
+            }
+
+            JSONArray mealArray = responseBody.getJSONArray("meals");
+
+            String[] mealIdArray = new String[mealArray.length()];
+            for (int i = 0; i < mealArray.length(); i++) {
+                JSONObject currentMeal = mealArray.getJSONObject(i);
+                mealIdArray[i] = currentMeal.getString("idMeal");
+            }
+
+            return getRecipesFromListOfIds(mealIdArray);
+
+        } catch (IOException | JSONException e) {
+            return null;
+        }
     }
 
     @Override
     public Recipe[] searchRecipesByName(String name) {
         return new Recipe[0];
+    }
+
+    private Boolean responseIsValid(Response response) {
+        if (response.body() == null) {
+            return false;
+        }
+        return response.code() == 200;
+    }
+
+    private Boolean dataInAPIResponse(JSONObject responseBody) {
+        return !(responseBody.isNull("meals"));
+    }
+
+    private Recipe constructRecipeFromMealJSON(JSONObject meal) {
+        return this.recipeFactory.create(
+                meal.getInt("idMeal"),
+                meal.getString("strMeal"),
+                meal.getString("strCategory"),
+                meal.getString("strInstructions"),
+                meal.getString("strArea"));
+    }
+
+    /**
+     * Method to get a list of recipes from a list of IDs. This is useful in situations where the API only returns a
+     * list of IDs, and not the actual recipe information itself (e.g. search by cuisine).
+     * @param idList list of strings, each of which is a valid ID.
+     * @return array of Recipe objects, each of which contains the recipe information. NOTE: there may be null objects,
+     * if we couldn't actually get the recipe objects for some reason.
+     */
+    private Recipe[] getRecipesFromListOfIds(String[] idList) {
+        CountDownLatch latch = new CountDownLatch(idList.length);
+        Recipe[] recipeArray = new Recipe[idList.length];
+        /* We set our thread pool size to 5 i.e. 5 simultaneous workers at a time */
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < idList.length; i++) {
+            final int insertionIndex = i;
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    recipeArray[insertionIndex] = getRecipeById(idList[insertionIndex]);
+                    latch.countDown();
+                }
+            });
+        }
+
+        try {
+            /* Ensures that we wait for everything to finish */
+            latch.await();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+
+        return recipeArray;
+
     }
 }
