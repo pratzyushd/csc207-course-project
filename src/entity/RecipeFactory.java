f65a7dbe4cf690f076fa0098f4876ed89e59199e
Jaeyong Lee <lee.j1533@gmail.com>
2023-11-11 22:01:05 -0500

    remove unnecessary import

--- src/entity/RecipeFactory.java
+++ src/entity/RecipeFactory.java
@@ -1,7 +1,5 @@
 package entity;
 
-import java.time.LocalDateTime;
-
 public interface RecipeFactory {
     Recipe create(int mealId, String name, String category, String instructions, String areaOfOrigin);
 }
