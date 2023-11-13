c7bb73d26391ec65760090bb119eec5f4005faae
Jaeyong Lee <lee.j1533@gmail.com>
2023-11-13 14:21:40 -0500

    add more detailed javadoc for common user factory

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -2,12 +2,12 @@ package entity;
 
 public class CommonUserFactory implements UserFactory {
     /**
-     * @param name
-     * @param password
+     * Create and return an instance of CommonUser.
+     * @param username an alphanumeric username assigned by the user.
+     * @param password the password the user will use with their username to log in.
      */
-
     @Override
-    public User create(String name, String password) {
-        return new CommonUser(name, password);
+    public User create(String username, String password) {
+        return new CommonUser(username, password);
     }
 }
