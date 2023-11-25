d7fdb64130b0391f6c76fb5f943f5d5a80d34430
Jaeyong Lee <lee.j1533@gmail.com>
2023-11-24 19:32:29 -0500

    implement prepare fail and success views in search by id presenter

--- src/interface_adapter/search_by_id/SearchByIdPresenter.java
+++ src/interface_adapter/search_by_id/SearchByIdPresenter.java
@@ -1,35 +1,48 @@
 package interface_adapter.search_by_id;
 
+import interface_adapter.SearchResultState;
+import interface_adapter.SearchResultViewModel;
 import interface_adapter.ViewManagerModel;
 import use_case.search_by_id.SearchIdOutputBoundary;
 import use_case.search_by_id.SearchIdOutputData;
 
+import javax.naming.directory.SearchResult;
+
 public class SearchByIdPresenter implements SearchIdOutputBoundary {
     private final SearchByIdViewModel searchByIdViewModel;
     private ViewManagerModel viewManagerModel;
+    private SearchResultViewModel searchResultViewModel;
 
-    public SearchByIdPresenter(ViewManagerModel viewManagerModel, SearchByIdViewModel searchByIdViewModel) {
+    public SearchByIdPresenter(ViewManagerModel viewManagerModel, SearchByIdViewModel searchByIdViewModel,
+                               SearchResultViewModel searchResultViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.searchByIdViewModel = searchByIdViewModel;
+        this.searchResultViewModel = searchResultViewModel;
     }
 
     @Override
     public void prepareSuccessView(SearchIdOutputData response) {
         System.out.println(response.getRecipe().getName());
-//        // On success, switch to the results view.
-//        SearchByIdState searchByIdState = searchByIdViewModel.getState();
-//        searchByIdState.setUsername(response.getUsername());
-//        this.loginViewModel.setState(loginState);
-//        loginViewModel.firePropertyChanged();
-//
-//        viewManagerModel.setActiveView(loginViewModel.getViewName());
-//        viewManagerModel.firePropertyChanged();
+
+        SearchResultState searchResultState = searchResultViewModel.getState();
+        searchResultState.setRecipeName(response.getRecipe().getName());
+        searchResultState.setRecipeCategory(response.getRecipe().getCategory());
+        searchResultState.setRecipeInstructions(response.getRecipe().getInstructions());
+        searchResultState.setRecipeAreaOfOrigin(response.getRecipe().getAreaOfOrigin());
+        searchResultState.setRecipeId(response.getRecipe().getMealId());
+
+        this.searchResultViewModel.setState(searchResultState);
+        System.out.println("\n" + searchResultViewModel.getState().getRecipeName());
+        searchResultViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(searchResultViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
     }
 
     @Override
-    public void prepareFailView(SearchIdOutputData error) {
-//        SignupState signupState = signupViewModel.getState();
-//        signupState.setUsernameError(error);
-//        signupViewModel.firePropertyChanged();
+    public void prepareFailView(String error) {
+        SearchByIdState searchByIdState = searchByIdViewModel.getState();
+        searchByIdState.setRecipeIdError(error);
+        searchByIdViewModel.firePropertyChanged();
     }
 }
