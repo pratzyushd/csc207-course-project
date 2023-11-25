b4ac475258b43b63cb6526bc60b590bdfbe1dbb7
Jaeyong Lee <lee.j1533@gmail.com>
2023-11-24 19:56:16 -0500

    implement search by id view model

--- src/interface_adapter/search_by_id/SearchByIdViewModel.java
+++ src/interface_adapter/search_by_id/SearchByIdViewModel.java
@@ -1,4 +1,36 @@
 package interface_adapter.search_by_id;
 
-public class SearchByIdViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class SearchByIdViewModel extends ViewModel {
+    public final String TITLE_LABEL = "Search Recipe By ID View";
+
+    private SearchByIdState state = new SearchByIdState();
+
+    public SearchByIdViewModel() {
+        super("search recipe by id view");
+    }
+
+    public void setState(SearchByIdState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the SearchById Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public SearchByIdState getState() {
+        return state;
+    }
 }
