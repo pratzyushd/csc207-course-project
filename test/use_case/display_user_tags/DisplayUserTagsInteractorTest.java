b13b437a1982fe07892f7f5cafa3f9c601a2090c
Pratyush Dwivedi <7183103-pratzyushd@users.noreply.gitlab.com>
2023-11-28 15:58:41 -0500

    Tests for DisplayUserTags use case

--- test/use_case/display_user_tags/DisplayUserTagsInteractorTest.java
+++ test/use_case/display_user_tags/DisplayUserTagsInteractorTest.java
@@ -1,4 +1,32 @@
 package use_case.display_user_tags;
 
+import data_access.InMemoryDAOMock;
+import org.junit.Test;
+import use_case.display_user_tags.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class DisplayUserTagsInteractorTest {
+    @Test
+    public void testUseCase() {
+        DisplayUserTagsDataAccessInterface mockDAO = new InMemoryDAOMock();
+        DisplayUserTagsOutputBoundary presenter = new DisplayUserTagsOutputBoundary() {
+            @Override
+            public void prepareSuccessView(DisplayUserTagsOutputData outputData) {
+                List<String> output = outputData.getUserTags();
+                assertEquals(2, output.size());
+                assertTrue(output.contains("tag1"));
+                assertTrue(output.contains("tag2"));
+            }
+        };
+
+        DisplayUserTagsInputData inputData = new DisplayUserTagsInputData();
+        DisplayUserTagsInputBoundary interactor = new DisplayUserTagsInteractor(presenter, mockDAO);
+        interactor.execute(inputData);
+    }
 }
