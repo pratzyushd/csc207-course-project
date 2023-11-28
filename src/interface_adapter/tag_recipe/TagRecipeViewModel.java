1e6ad3e73712ad304071eecb94cf5e692311e64e
Jaeyong Lee <lee.j1533@gmail.com>
2023-11-27 22:27:18 -0500

    add tag recipe view model and tag recipe state

---
+++ src/interface_adapter/tag_recipe/TagRecipeViewModel.java
@@ -0,0 +1,33 @@
+package interface_adapter.tag_recipe;
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class TagRecipeViewModel extends ViewModel {
+    private TagRecipeState state = new TagRecipeState();
+
+    public TagRecipeViewModel() {
+        super("tag recipe view");
+    }
+
+    public void setState(TagRecipeState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the presenter will call to let the ViewModel know to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public TagRecipeState getState() {
+        return state;
+    }
+}
