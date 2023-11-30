ff7103f224a6361f5b7fe111d09c89ecc00f1c93
Jaeyong Lee <lee.j1533@gmail.com>
2023-11-30 17:37:19 -0500

    implement search recipe by name view model

--- src/interface_adapter/search_by_name/SearchByNameViewModel.java
+++ src/interface_adapter/search_by_name/SearchByNameViewModel.java
@@ -1,4 +1,34 @@
 package interface_adapter.search_by_name;
 
-public class SearchByNameViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class SearchByNameViewModel extends ViewModel {
+    private SearchByNameState state = new SearchByNameState();
+
+    public SearchByNameViewModel() {
+        super("search recipe by name view");
+    }
+
+    public void setState(SearchByNameState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the SearchByName Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public SearchByNameState getState() {
+        return state;
+    }
 }
